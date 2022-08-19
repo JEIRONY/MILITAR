f89cf8de0891f72035f265950128856327794eb0
JEIRONY <82543429+JEIRONY@users.noreply.github.com>
2022-08-18 20:36:11 -0500

    Update owner-banuser.js

--- plugins/owner-banuser.js
+++ plugins/owner-banuser.js
@@ -5,7 +5,6 @@ let who
 if (m.isGroup) who = m.mentionedJid[0]
 else who = m.chat
 if (!who) throw '*[❗INFO❗] INGRESA EL @tag DE ALGÚN USUARIO*'
-let users = global.db.data.users
 users[who].banned = true
 conn.reply(m.chat, `*[❗INFO❗] EL USUARIO FUE BANEADO CON ÉXITO*\n*—◉ EL USUARIO YA NO PODRÁ USAR EL BOT HASTA QUE SEA DESBANEADO*`, m)
 }
