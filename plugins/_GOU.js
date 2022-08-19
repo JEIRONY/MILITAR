2a1fb5abf7f9a14e7a32088b9ee35001e3f181c5
JEIRONY <82543429+JEIRONY@users.noreply.github.com>
2022-08-18 19:52:09 -0500

    Add files via upload

---
+++ plugins/_GOU.js
@@ -0,0 +1,11 @@
+let handler = async (m) => {
+    m.reply(
+    `*_➤ Asociación de grupos de anime S.A._*
+
+   POR AHORA NO TENEMOS GRUPOS DE ANIME 
+    `
+    )}
+    handler.command = /^(asociaciongruposotakus|asociacion de grupos otakus|GOU)$/i
+    handler.fail = null
+    export default handler
+    
